47e4cf46b1b72a3ba3cf841f841c7e668d058512
Matthew Brown <mattfdbrown@gmail.com>
2022-02-06 12:10:57 -0800

    use tsconfig-paths
    allows for aliasing imports

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "tsconfig-paths/register";
 
 import { HardhatUserConfig } from "hardhat/config";
 
