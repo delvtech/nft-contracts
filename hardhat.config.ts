7bb2ed5ff104fa8c00ef3aa983c3d9ac79b94f4c
Matthew Brown <mattfdbrown@gmail.com>
2022-03-09 15:59:19 -0800

    add scripts to update things

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,15 @@
-import "tsconfig-paths/register";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
+import "dotenv/config";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "tsconfig-paths/register";
 
-import { HardhatUserConfig } from "hardhat/config";
+import { ethers, providers } from "ethers";
 import { existsSync, readFileSync } from "fs";
+import { HardhatUserConfig, task, types } from "hardhat/config";
+import { updateMerkleRoot } from "scripts/updateMerkleRoot";
+import { updateBaseURI } from "scripts/updateBaseURI";
 
 const EXPECTED_ACCOUNT_FILE = "accounts.json";
 
@@ -23,6 +27,52 @@ const getAddresses = (): string[] | undefined => {
 };
 
 const addresses = getAddresses();
+
+const { PRIVATE_KEY, ALCHEMY_GOERLI_RPC_HOST } = process.env;
+
+task("updateMerkleRoot", "updates the merkle root")
+  .addParam("merkleRoot", "The new merkle root", undefined, types.string)
+  .setAction(async (taskArgs: { merkleRoot: string }) => {
+    const { merkleRoot } = taskArgs;
+
+    if (!PRIVATE_KEY) {
+      console.log("ERROR: no private key provided");
+      return;
+    }
+
+    const localhostProvider = new providers.JsonRpcProvider(
+      ALCHEMY_GOERLI_RPC_HOST
+    );
+
+    const owner = new ethers.Wallet(PRIVATE_KEY, localhostProvider);
+
+    const ownerAddress = owner.address;
+    console.log("ownerAddress", ownerAddress);
+
+    await updateMerkleRoot(owner, merkleRoot);
+  });
+
+task("updateBaseURI", "updates the base URI of the token contract")
+  .addParam("baseURI", "The new base URI", undefined, types.string)
+  .setAction(async (taskArgs: { baseURI: string }) => {
+    const { baseURI } = taskArgs;
+
+    if (!PRIVATE_KEY) {
+      console.log("ERROR: no private key provided");
+      return;
+    }
+
+    const localhostProvider = new providers.JsonRpcProvider(
+      ALCHEMY_GOERLI_RPC_HOST
+    );
+
+    const owner = new ethers.Wallet(PRIVATE_KEY, localhostProvider);
+
+    const ownerAddress = owner.address;
+    console.log("ownerAddress", ownerAddress);
+
+    await updateBaseURI(owner, baseURI);
+  });
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
