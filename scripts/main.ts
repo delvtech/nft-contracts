a83bd7225e96c1a7e031f6b349e2f5ebcb64f4ea
Matthew Brown <mattfdbrown@gmail.com>
2022-04-03 12:00:52 -1000

    update scripts

--- scripts/main.ts
+++ scripts/main.ts
@@ -4,10 +4,20 @@ import { providers, Wallet } from "ethers";
 import { deployElfNFT } from "./deployElfNFT";
 import { deployMinter } from "./deployMinter";
 
-const { PRIVATE_KEY, MERKLE_ROOT, ALCHEMY_GOERLI_RPC_HOST } = process.env;
+const { PRIVATE_KEY, MERKLE_ROOT, ALCHEMY_RPC_HOST, CHAIN_ID } = process.env;
 
-const GOERLI_CHAIN_ID = 5;
 async function main() {
+  const chainID = Number(CHAIN_ID) || 1;
+  if (!ALCHEMY_RPC_HOST) {
+    console.error("no provider url");
+    return;
+  }
+
+  if (!CHAIN_ID) {
+    console.error("no chain id provided");
+    return;
+  }
+
   if (!PRIVATE_KEY) {
     console.error("no private key provided for deployer");
     return;
@@ -18,26 +28,34 @@ async function main() {
     return;
   }
 
-  const provider = new providers.JsonRpcProvider(ALCHEMY_GOERLI_RPC_HOST);
+  if (chainID !== 9000) {
+    return;
+  }
+
+  const provider = new providers.JsonRpcProvider(ALCHEMY_RPC_HOST);
   const deployer = new Wallet(PRIVATE_KEY, provider);
 
   // deploy elf nft contract
   const nftContract = await deployElfNFT(
     deployer,
-    GOERLI_CHAIN_ID,
+    chainID,
     "Elfie NFT",
     "ELFNFT",
-    "ipfs.io/ipfs/abc"
+    "ipfs://QmcsGvPN4yyPwA7fECzgPjX8nyfaCs3Rr9aW3MdnXm7M6S"
   );
 
   // authorize the deployer before we transfer ownership to the minter contract
   // so it can set the baseURI
   await nftContract.connect(deployer).authorize(deployer.address);
-  console.log("deployer address ", deployer, " authorized on nft contract");
+  console.log(
+    "deployer address ",
+    deployer.address,
+    " authorized on nft contract"
+  );
 
   const minterContract = await deployMinter(
     deployer,
-    GOERLI_CHAIN_ID,
+    chainID,
     nftContract.address,
     MERKLE_ROOT
   );
---
+++ scripts/mint.ts
@@ -0,0 +1,96 @@
+import "dotenv/config";
+
+import { ethers, providers } from "ethers";
+import { AbiCoder } from "ethers/lib/utils";
+import { makeMerkle } from "test/helpers/makeMerkle";
+import { Minter__factory } from "typechain-types/factories/Minter__factory";
+
+const { PRIVATE_KEY, MERKLE_ROOT, ALCHEMY_RPC_HOST, CHAIN_ID } = process.env;
+
+async function main() {
+  const chainId = Number(CHAIN_ID) || 1;
+  console.log("PRIVATE_KEY", PRIVATE_KEY);
+  console.log("MERKLE_ROOT", MERKLE_ROOT);
+  console.log("ALCHEMY_RPC_HOST", ALCHEMY_RPC_HOST);
+  console.log("chainId", chainId);
+  if (!ALCHEMY_RPC_HOST) {
+    console.error("no provider url");
+    return;
+  }
+
+  if (!CHAIN_ID) {
+    console.error("no chain id provided");
+    return;
+  }
+
+  if (!PRIVATE_KEY) {
+    console.error("no private key provided for deployer");
+    return;
+  }
+
+  if (!MERKLE_ROOT) {
+    console.error("no merkle root provided for minter contract");
+    return;
+  }
+  // rinkeby minter address;
+  const minterContractAddress = "0x887EebeB6C6aC9F8D85FA56171797b91A4024e06";
+
+  const localhostProvider = new providers.JsonRpcProvider(ALCHEMY_RPC_HOST);
+
+  const owner = new ethers.Wallet(PRIVATE_KEY, localhostProvider);
+
+  const ownerAddress = owner.address;
+  console.log("ownerAddress", ownerAddress);
+
+  const merkleTree = await makeMerkle();
+  const merkleRoot = merkleTree.getHexRoot();
+  console.log("merkleRoot", merkleRoot);
+
+  const leaves = merkleTree.getLeaves();
+  const proof = merkleTree.getHexProof(leaves[0]);
+  console.log("proof", proof);
+
+  const coder = new ethers.utils.AbiCoder();
+  console.log("coder");
+  try {
+    const encoded = coder.encode(["bytes32", "bytes32"], proof);
+    const bytes = encoded.slice(2); //remove 0x prepend
+    console.log("bytes", bytes);
+    console.log("bytes", bytes.length);
+    // put into 32 byte chuncks
+    const btyes32array = sliceIntoChunks(bytes, 64);
+    console.log("btyes32array", btyes32array);
+  } catch (error) {
+    console.log("error", error);
+  }
+
+  const minterContract = Minter__factory.connect(minterContractAddress, owner);
+  console.log("minterContract");
+
+  try {
+    const root = await minterContract.merkleRoot();
+    console.log("root", root);
+    const tx = await minterContract.mint(0, proof);
+  } catch (error) {
+    console.log("error", error);
+  }
+
+  console.log("minted elf 0");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+function sliceIntoChunks(arr: any[] | string, chunkSize: number) {
+  const res = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    res.push(chunk);
+  }
+  return res;
+}
